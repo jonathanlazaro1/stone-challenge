5413196371eb859ca00ad22adafceb96b89e1661
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2020-08-10 23:56:23 -0400

    feat: create Invoice faker

---
+++ core/models/invoice_faker.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"time"
+
+	"github.com/brianvoe/gofakeit/v5"
+)
+
+func substr(str string, maxLen int) string {
+	runes := []rune(str)
+	if len(runes) < maxLen {
+		return string(runes)
+	}
+	return string(runes[:maxLen-1])
+}
+
+// MakeFakeInvoice will generate an Invoice with fake populated data
+func MakeFakeInvoice() Invoice {
+	in := NewInvoice()
+	in.ID = gofakeit.Number(1, 100000)
+
+	currYear := time.Now().Year()
+	in.ReferenceYear = gofakeit.Number(currYear-5, currYear-1)
+	in.ReferenceMonth = gofakeit.Number(1, 12)
+
+	in.Document = substr(gofakeit.Phrase(), 14)
+	in.Description = substr(gofakeit.Phrase(), 256)
+	in.Amount = gofakeit.Price(0.01, 1000000)
+
+	return in
+}
