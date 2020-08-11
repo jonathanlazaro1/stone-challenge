8521152d1a9a6a7214999bd551539b632b6b701a
Jonathan Lazzaro <jonathan.lazaro1@gmail.com>
2020-08-11 09:21:54 -0400

    feat: create app entrypoint

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/jonathanlazaro1/stone-challenge/infrastructure/router"
+)
+
+func main() {
+	r := router.Router()
+
+	fmt.Println("Starting server on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
