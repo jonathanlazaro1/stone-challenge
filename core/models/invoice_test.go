e4e317204e944d2bd6ffa92423c902d4531156bf
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2020-08-10 22:12:31 -0400

    test: better messages for failed tests

--- core/models/invoice_test.go
+++ core/models/invoice_test.go
@@ -10,11 +10,11 @@ func TestNewInvoice(t *testing.T) {
 
 	// A new Invoice should be active
 	if isActiveWanted != sut.IsActive {
-		t.Errorf("NewInvoice failed: IsActive should be %v, but was %v", isActiveWanted, sut.IsActive)
+		t.Errorf("NewInvoice failed, IsActive: got %v, wanted %v", isActiveWanted, sut.IsActive)
 	}
 
 	// A new Invoice's CreatedAt should not be zero
 	if sut.CreatedAt.IsZero() {
-		t.Errorf("NewInvoice failed: CreatedAt should be the current date and time, but was %v", sut.CreatedAt)
+		t.Errorf("NewInvoice failed, CreatedAt: wanted current date and time, got %v", sut.CreatedAt)
 	}
 }
