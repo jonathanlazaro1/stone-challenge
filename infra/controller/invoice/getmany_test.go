9d9f1cd96ec48a87ca0284f876f80197d531e620
Jonathan Lazzaro <jonathan.lazaro1@gmail.com>
2020-08-12 09:36:25 -0400

    test: unparseable items per page or page should return 400 Bad Request

--- infra/controller/invoice/getmany_test.go
+++ infra/controller/invoice/getmany_test.go
@@ -1,13 +1,16 @@
 package invoice
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 //TODO: improve tests to check if passed parameteres are coming to handler as expected
+//TODO: do tests pass with a sensitive case param?
 
+// ==================== NO PARAMS ====================
 func TestInvoiceGetManyHandlerWithoutParameters(t *testing.T) {
 	verb := "GET"
 	endpoint := "/api/v1/invoice"
@@ -24,7 +27,7 @@ func TestInvoiceGetManyHandlerWithoutParameters(t *testing.T) {
 
 	statusWant := http.StatusOK
 	if statusGotten := rr.Code; statusGotten != statusWant {
-		t.Errorf("%v %v failed: wanted body to be %v, but got %v", verb, endpoint, statusWant, statusGotten)
+		t.Errorf("%v %v failed: wanted status to be %v, but got %v", verb, endpoint, statusWant, statusGotten)
 	}
 
 	contentTypeWant := `application/json`
@@ -33,6 +36,8 @@ func TestInvoiceGetManyHandlerWithoutParameters(t *testing.T) {
 		t.Errorf("%v %v failed: wanted Content-Type to be \"%v\", but got  \"%v\"", verb, endpoint, contentTypeWant, contentTypeGotten)
 	}
 }
+
+// ==================== PAGE NUMBER ====================
 
 func TestInvoiceGetManyHandlerWithPageNumber(t *testing.T) {
 	verb := "GET"
@@ -50,7 +55,7 @@ func TestInvoiceGetManyHandlerWithPageNumber(t *testing.T) {
 
 	statusWant := http.StatusOK
 	if statusGotten := rr.Code; statusGotten != statusWant {
-		t.Errorf("%v %v failed: wanted body to be %v, but got %v", verb, endpoint, statusWant, statusGotten)
+		t.Errorf("%v %v failed: wanted status to be %v, but got %v", verb, endpoint, statusWant, statusGotten)
 	}
 
 	contentTypeWant := `application/json`
@@ -59,6 +64,34 @@ func TestInvoiceGetManyHandlerWithPageNumber(t *testing.T) {
 		t.Errorf("%v %v failed: wanted Content-Type to be \"%v\", but got  \"%v\"", verb, endpoint, contentTypeWant, contentTypeGotten)
 	}
 }
+
+func TestInvoiceGetManyHandlerWithUnparseablePageNumber(t *testing.T) {
+	verb := "GET"
+	endpoint := "/api/v1/invoice?p=impossible"
+	req, err := http.NewRequest(verb, endpoint, nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetManyHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	statusWant := http.StatusBadRequest
+	if statusGotten := rr.Code; statusGotten != statusWant {
+		t.Errorf("%v %v failed: wanted status to be %v, but got %v", verb, endpoint, statusWant, statusGotten)
+	}
+
+	bodyWant := errParsingPageNumber
+	bodyGotten := rr.Body.String()
+	if bodyGotten != bodyWant {
+		t.Errorf("%v %v failed: wanted Content-Type to be \"%v\", but got \"%v\"", verb, endpoint, bodyWant, bodyGotten)
+	}
+}
+
+// ==================== ITEMS PER PAGE ====================
 
 func TestInvoiceGetManyHandlerWithItemsPerPage(t *testing.T) {
 	verb := "GET"
@@ -76,12 +109,64 @@ func TestInvoiceGetManyHandlerWithItemsPerPage(t *testing.T) {
 
 	statusWant := http.StatusOK
 	if statusGotten := rr.Code; statusGotten != statusWant {
-		t.Errorf("%v %v failed: wanted body to be %v, but got %v", verb, endpoint, statusWant, statusGotten)
+		t.Errorf("%v %v failed: wanted status to be %v, but got %v", verb, endpoint, statusWant, statusGotten)
 	}
 
 	contentTypeWant := `application/json`
 	contentTypeGotten := rr.Header().Get("Content-Type")
 	if contentTypeGotten != contentTypeWant {
 		t.Errorf("%v %v failed: wanted Content-Type to be \"%v\", but got  \"%v\"", verb, endpoint, contentTypeWant, contentTypeGotten)
+	}
+}
+
+func TestInvoiceGetManyHandlerWithUnparseableItemsPerPage(t *testing.T) {
+	verb := "GET"
+	endpoint := "/api/v1/invoice?ipp=notagreatitemsperpage"
+	req, err := http.NewRequest(verb, endpoint, nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetManyHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	statusWant := http.StatusBadRequest
+	if statusGotten := rr.Code; statusGotten != statusWant {
+		t.Errorf("%v %v failed: wanted status to be %v, but got %v", verb, endpoint, statusWant, statusGotten)
+	}
+
+	bodyWant := errParsingItemsPerPage
+	bodyGotten := rr.Body.String()
+	if bodyGotten != bodyWant {
+		t.Errorf("%v %v failed: wanted Content-Type to be \"%v\", but got \"%v\"", verb, endpoint, bodyWant, bodyGotten)
+	}
+}
+
+func TestInvoiceGetManyHandlerWithExcessiveItemsPerPage(t *testing.T) {
+	verb := "GET"
+	endpoint := "/api/v1/invoice?ipp=1445332"
+	req, err := http.NewRequest(verb, endpoint, nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetManyHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	statusWant := http.StatusBadRequest
+	if statusGotten := rr.Code; statusGotten != statusWant {
+		t.Errorf("%v %v failed: wanted body to be %v, but got %v", verb, endpoint, statusWant, statusGotten)
+	}
+
+	bodyWant := fmt.Sprintf(errMaxItemsPerPageAllowed, maxItemsPerPage)
+	bodyGotten := rr.Body.String()
+	if bodyGotten != bodyWant {
+		t.Errorf("%v %v failed: wanted Content-Type to be \"%v\", but got \"%v\"", verb, endpoint, bodyWant, bodyGotten)
 	}
 }
