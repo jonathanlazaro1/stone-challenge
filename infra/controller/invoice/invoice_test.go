e07995c4798b441bdd32f92b0e7633d7170d6880
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2020-08-11 23:20:10 -0400

    test: GetMany should return 200 OK when page params is passed correctly

--- infra/controller/invoice/invoice_test.go
+++ infra/controller/invoice/invoice_test.go
@@ -6,15 +6,43 @@ import (
 	"testing"
 )
 
-func TestInvoiceIndexHandler(t *testing.T) {
+func TestInvoiceGetManyHandlerWithoutParameters(t *testing.T) {
 	verb := "GET"
 	endpoint := "/api/v1/invoice"
 	req, err := http.NewRequest(verb, endpoint, nil)
+
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(IndexHandler)
+	handler := http.HandlerFunc(GetManyHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	statusWant := http.StatusOK
+	if statusGotten := rr.Code; statusGotten != statusWant {
+		t.Errorf("%v %v failed: wanted body to be %v, but got %v", verb, endpoint, statusWant, statusGotten)
+	}
+
+	contentTypeWant := `application/json`
+	contentTypeGotten := rr.Header().Get("Content-Type")
+	if contentTypeGotten != contentTypeWant {
+		t.Errorf("%v %v failed: wanted Content-Type to be \"%v\", but got  \"%v\"", verb, endpoint, contentTypeWant, contentTypeGotten)
+	}
+}
+
+func TestInvoiceGetManyHandler(t *testing.T) {
+	verb := "GET"
+	endpoint := "/api/v1/invoice?p=2"
+	req, err := http.NewRequest(verb, endpoint, nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetManyHandler)
 
 	handler.ServeHTTP(rr, req)
 
