1f2aac787399273b42b4e0b0d87a71cda80f4e19
Jonathan Lazzaro <jonathan.lazaro1@gmail.com>
2020-08-12 09:35:50 -0400

    feat: increment fake repo items size

--- infra/service/invoice.go
+++ infra/service/invoice.go
@@ -7,7 +7,7 @@ import (
 
 // BuildInvoiceService builds a new InvoiceInteractor with the specified repository
 func BuildInvoiceService() *invoice.Interactor {
-	repo := test.MockInvoiceRepository(20)
+	repo := test.MockInvoiceRepository(50)
 	service := invoice.NewInteractor(repo)
 
 	return service
