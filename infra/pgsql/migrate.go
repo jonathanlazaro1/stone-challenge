2ead871f9df5daa4451d7ec0c1ee28f2bea7c728
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2020-08-12 21:33:17 -0400

    feat: migration is working

--- infra/pgsql/migrate.go
+++ infra/pgsql/migrate.go
@@ -1,24 +1,51 @@
 package pgsql
 
 import (
-	"fmt"
+	"database/sql"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 )
 
-// Migrate tries to update the PGSQL database that is set on environment, to put it on the DB state needed by application to work with
+// execSQL executes an SQL statement against a DB connection
+func execSQL(db *sql.DB, sql string) error {
+	_, err := db.Exec(sql)
+	return err
+}
+
+// Migrate tries to update the PGSQL database that is set on environment, taking it to the state needed by application to work with
 func Migrate() {
-	var files []string
-	migrationsDir := "./migrations/"
+	db := CreateConnection()
+	defer db.Close()
+
+	var sqlFiles []string
+	migrationsDir := "./infra/pgsql/migrations/"
+
+	// Get all .sql files on migrations folder
 	err := filepath.Walk(migrationsDir, func(path string, info os.FileInfo, err error) error {
-		files = append(files, path)
+		if info.IsDir() || filepath.Ext(path) != ".sql" {
+			return nil
+		}
+		sqlFiles = append(sqlFiles, path)
 		return nil
 	})
 	if err != nil {
-		log.Fatalln("Could not execute migration")
+		log.Fatalln("Could not execute PGSQL DB migration")
 	}
-	for _, file := range files {
-		fmt.Println(file)
+
+	// Go through the .sql files found on folder to exec the SQL statements
+	for _, file := range sqlFiles {
+		bytes, err := ioutil.ReadFile(file)
+		if err != nil {
+			log.Fatalf("Could not read file %v", file)
+		}
+		sql := string(bytes)
+
+		err = execSQL(db, sql)
+		if err != nil {
+			log.Fatalf("Could not execute migration %v: %v", file, err)
+		}
 	}
+	log.Println("Migrations applied successfully")
 }
