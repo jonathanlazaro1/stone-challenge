1856106352c31f95a3599e2d16c379c5c9b7a01d
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2020-08-12 23:41:28 -0400

    fix: nullable time to deactivated_at

--- domain/invoice/invoice.go
+++ domain/invoice/invoice.go
@@ -1,8 +1,23 @@
 package invoice
 
 import (
+	"database/sql"
+	"encoding/json"
 	"time"
 )
+
+// JSONNullTime represents a time.Time type that can be null
+type JSONNullTime struct {
+	sql.NullTime
+}
+
+// MarshalJSON parses a NullableTimeType into a JSON (ISO 8601) type or null
+func (v JSONNullTime) MarshalJSON() ([]byte, error) {
+	if v.Valid {
+		return json.Marshal(v.Time)
+	}
+	return json.Marshal(nil)
+}
 
 // Invoice represents an Invoice entity. A new Invoice must be obtained by using NewInvoice method
 type Invoice struct {
@@ -14,7 +29,7 @@ type Invoice struct {
 	Amount         float64
 	IsActive       bool
 	CreatedAt      time.Time
-	DeactivatedAt  time.Time
+	DeactivatedAt  JSONNullTime
 }
 
 // NewInvoice creates a new instance of an Invoice
