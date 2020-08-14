298d170356be5cfd3c2102adf57e71f9209145b7
Jonathan Lazzaro <jonathan.lazaro1@gmail.com>
2020-08-14 11:33:42 -0400

    done: post Invoice

--- infra/controller/invoice/post.go
+++ infra/controller/invoice/post.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
+	domain "github.com/jonathanlazaro1/stone-challenge/domain/invoice"
+	"github.com/jonathanlazaro1/stone-challenge/infra/service"
 )
 
 // PostModel represents an Invoice model to be persisted to an Invoice
@@ -29,6 +32,18 @@ func (m PostModel) Validate() error {
 	)
 }
 
+// ToInvoice converts a PostModel to an Invoice instance
+func (m PostModel) ToInvoice() domain.Invoice {
+	invoice := domain.NewInvoice()
+	invoice.ReferenceYear = m.ReferenceYear
+	invoice.ReferenceMonth = m.ReferenceMonth
+	invoice.Document = m.Document
+	invoice.Description = m.Description
+	invoice.Amount = m.Amount
+
+	return invoice
+}
+
 // PostHandler handles a request to post an Invoice
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 	var model PostModel
@@ -46,5 +61,15 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+	service := service.BuildInvoiceService()
+
+	id, err := service.Add(model.ToInvoice())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, "Couldn't create Invoice")
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	io.WriteString(w, strconv.Itoa(id))
 }
