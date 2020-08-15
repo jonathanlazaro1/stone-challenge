e184567448e6671364cdb1e5e7fb55baa123704a
Jonathan Lazzaro <jonathan.lazaro1@gmail.com>
2020-08-15 11:18:32 -0400

    fix: CopyIfNotNil needs to check if src field value is a pointer

--- helpers/structhelpers.go
+++ helpers/structhelpers.go
@@ -2,14 +2,22 @@ package helpers
 
 import "reflect"
 
-// CopyIfNotNil copies struct src fields that have a match on dest, if their pointer values are not nil
+// CopyIfNotNil copies struct src fields that have a name match on dest, if their src pointer values are not nil
 func CopyIfNotNil(src interface{}, dest interface{}) {
 	destReflect := reflect.ValueOf(dest).Elem()
 	srcReflect := reflect.ValueOf(src).Elem()
 	for i := 0; i < srcReflect.NumField(); i++ {
-		destField := destReflect.Field(i)
+		destField := destReflect.FieldByName(srcReflect.Type().Field(i).Name)
+		if !destField.IsValid() {
+			continue
+		}
 		srcFieldValue := reflect.Value(srcReflect.Field(i))
-		if !srcFieldValue.IsNil() {
+		if srcFieldValue.IsNil() {
+			continue
+		}
+		if srcFieldValue.Kind() == reflect.Ptr {
+			destField.Set(srcFieldValue.Elem())
+		} else {
 			destField.Set(srcFieldValue)
 		}
 	}
