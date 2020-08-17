2bff97d76e815afd3c39fbffcb4e912b2befc4f8
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2020-08-17 14:11:06 -0400

    refactor: better name for Invoice service struct

--- infra/service/invoice.go
+++ infra/service/invoice.go
@@ -5,33 +5,33 @@ import (
 	"github.com/jonathanlazaro1/stone-challenge/usecase"
 )
 
-// InvoiceService handles view layer calls to fetch and modify Invoices
-type InvoiceService struct {
+// Invoice represents a service that handles view layer calls to fetch and modify Invoices
+type Invoice struct {
 	interactor usecase.InvoiceInteractor
 }
 
-func newInvoiceService(repo usecase.InvoiceRepository) *InvoiceService {
-	return &InvoiceService{interactor: *usecase.NewInvoiceInteractor(repo)}
+func newInvoiceService(repo usecase.InvoiceRepository) *Invoice {
+	return &Invoice{interactor: *usecase.NewInvoiceInteractor(repo)}
 }
 
 // GetMany returns an array of Invoices, according to the given arguments
-func (svc *InvoiceService) GetMany(itemsPerPage int, page int, filterBy map[string]string, sortBy map[string]bool) ([]domain.Invoice, int64, error) {
+func (svc *Invoice) GetMany(itemsPerPage int, page int, filterBy map[string]string, sortBy map[string]bool) ([]domain.Invoice, int64, error) {
 	return svc.interactor.GetMany(itemsPerPage, page, filterBy, sortBy)
 }
 
 // Get an Invoice, given its Id
-func (svc *InvoiceService) Get(id int) (*domain.Invoice, error) {
+func (svc *Invoice) Get(id int) (*domain.Invoice, error) {
 	return svc.interactor.Get(id)
 }
 
 // Add creates a new Invoice
-func (svc *InvoiceService) Add(invoice domain.Invoice) (int, error) {
+func (svc *Invoice) Add(invoice domain.Invoice) (int, error) {
 	return svc.interactor.Add(invoice)
 }
 
 // Update replaces the values on the Invoice held by id, using the new ones that came from request.
 // It ignores server-controled ones, such as CreatedAt, IsActive and UpdatedAt
-func (svc *InvoiceService) Update(id int, postModel PostModel) (*domain.Invoice, error) {
+func (svc *Invoice) Update(id int, postModel PostModel) (*domain.Invoice, error) {
 	currentInvoice, err := svc.interactor.Get(id)
 	if err != nil {
 		return nil, err
@@ -55,7 +55,7 @@ func (svc *InvoiceService) Update(id int, postModel PostModel) (*domain.Invoice,
 }
 
 // Delete makes an Invoice inactive, which makes it unable to be retrieved/maintained
-func (svc *InvoiceService) Delete(id int) (bool, error) {
+func (svc *Invoice) Delete(id int) (bool, error) {
 	invoice, err := svc.interactor.Get(id)
 	if err != nil {
 		return false, err
--- infra/service/service.go
+++ infra/service/service.go
@@ -4,8 +4,8 @@ import (
 	"github.com/jonathanlazaro1/stone-challenge/infra/pgsql"
 )
 
-// BuildInvoiceService builds an instance of InvoiceService
-func BuildInvoiceService() *InvoiceService {
+// BuildInvoiceService builds an instance of Invoice
+func BuildInvoiceService() *Invoice {
 	repo := pgsql.GetInvoiceRepository()
 	return newInvoiceService(repo)
 }
