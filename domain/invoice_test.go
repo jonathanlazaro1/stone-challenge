bb181e4a922e2c907fdffe0a3572a4ecfbdbdacc
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2020-08-17 12:09:09 -0400

    test: deactivated invoice has to be inactive and have deactivated_at value

--- domain/invoice_test.go
+++ domain/invoice_test.go
@@ -18,3 +18,19 @@ func TestNewInvoice(t *testing.T) {
 		t.Errorf("NewInvoice failed, CreatedAt: wanted current date and time, got %v", sut.CreatedAt)
 	}
 }
+
+func TestDeactivatedInvoice(t *testing.T) {
+	sut := NewInvoice()
+	sut.Deactivate()
+	isActiveWanted := false
+
+	// A deactivated Invoice should be... deactivated
+	if isActiveWanted != sut.IsActive {
+		t.Errorf("DeactivateInvoice failed, IsActive: got %v, wanted %v", isActiveWanted, sut.IsActive)
+	}
+
+	// A new Invoice's CreatedAt should not be zero
+	if sut.DeactivatedAt.Time.IsZero() {
+		t.Errorf("DeactivateInvoice failed, DeactivatedAt: wanted current date and time, got %v", sut.DeactivatedAt.Time)
+	}
+}
