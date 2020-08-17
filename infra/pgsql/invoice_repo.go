c2d86b89dcd7500c49685fc4539b594861707fd5
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2020-08-17 12:07:35 -0400

    refactor: better and simplified queries

--- infra/pgsql/invoice_repo.go
+++ infra/pgsql/invoice_repo.go
@@ -21,21 +21,23 @@ func GetInvoiceRepository() usecase.InvoiceRepository {
 	return &invoiceRepository{}
 }
 
-//TODO: change document filter to be "like" instead of "equal"
 // GetMany fetches all invoices found on DB table invoice, according to the parameters given. It also returns the total count for the query made
 func (repo *invoiceRepository) GetMany(itemsPerPage int, page int, filterBy map[string]string, sortBy map[string]bool) ([]domain.Invoice, int64, error) {
 	db := CreateConnection()
 	defer db.Close()
 	database := goqu.New("postgresql", db)
 
-	goquWhere := goqu.Ex{"is_active": true}
-	for k, v := range filterBy {
-		goquWhere[k] = v
-	}
-
 	goquSQL := database.
 		From("invoice").
-		Where(goquWhere)
+		Where(goqu.C("is_active").Eq(true))
+
+	for k, v := range filterBy {
+		if k == "document" {
+			goquSQL = goquSQL.Where(goqu.C(k).Like(fmt.Sprintf("%c%v%c", '\u0025', v, '\u0025')))
+		} else {
+			goquSQL = goquSQL.Where(goqu.C(k).Eq(v))
+		}
+	}
 
 	// Counting rows
 	count, err := goquSQL.Count()
@@ -54,39 +56,11 @@ func (repo *invoiceRepository) GetMany(itemsPerPage int, page int, filterBy map[
 		}
 	}
 
-	sql, _, _ := goquSQL.ToSQL()
-
-	rows, err := database.Query(sql)
-
-	if err != nil {
-		log.Printf("Unable to fetch invoices. %v", err)
+	invoices := []domain.Invoice{}
+	if err := goquSQL.ScanStructs(&invoices); err != nil {
 		return nil, count, err
 	}
-
-	defer rows.Close()
-	invoices := []domain.Invoice{}
-
-	for rows.Next() {
-		var invoice domain.Invoice
-		err = rows.Scan(
-			&invoice.ID,
-			&invoice.ReferenceYear,
-			&invoice.ReferenceMonth,
-			&invoice.Document,
-			&invoice.Description,
-			&invoice.Amount,
-			&invoice.IsActive,
-			&invoice.CreatedAt,
-			&invoice.DeactivatedAt)
-
-		if err != nil {
-			log.Printf("Unable to fetch invoices. %v", err)
-			return nil, count, err
-		}
-
-		invoices = append(invoices, invoice)
-	}
-	return invoices, count, err
+	return invoices, count, nil
 }
 
 // Get finds an Invoice, given its Id
@@ -95,36 +69,26 @@ func (repo *invoiceRepository) Get(id int) (*domain.Invoice, error) {
 	defer db.Close()
 	database := goqu.New("postgresql", db)
 
-	goquWhere := goqu.Ex{
-		"is_active": true,
-		"id":        id,
-	}
-
-	goquSQL := database.From("invoice").Where(goquWhere)
+	goquSQL := database.
+		From("invoice").
+		Where(
+			goqu.C("is_active").Eq(true),
+			goqu.C("id").Eq(id))
 
 	count, err := goquSQL.Count()
 	if count < 1 {
 		return nil, nil
 	}
 
-	sql, _, _ := goquSQL.ToSQL()
 	var invoice domain.Invoice
-
-	err = database.QueryRow(sql).Scan(
-		&invoice.ID,
-		&invoice.ReferenceYear,
-		&invoice.ReferenceMonth,
-		&invoice.Document,
-		&invoice.Description,
-		&invoice.Amount,
-		&invoice.IsActive,
-		&invoice.CreatedAt,
-		&invoice.DeactivatedAt)
-
+	rowWasFound, err := goquSQL.ScanStruct(&invoice)
 	if err != nil {
-		log.Printf("Unable to fetch invoices. %v", err)
 		return nil, err
 	}
+	if !rowWasFound {
+		return nil, nil
+	}
+
 	return &invoice, nil
 }
 
@@ -145,16 +109,13 @@ func (repo *invoiceRepository) Add(invoice domain.Invoice) (int, error) {
 		"deactivated_at":  invoice.DeactivatedAt,
 	}
 
-	goquSQL := database.From("invoice").Insert().Rows(record).Returning("id")
-	sql, _, _ := goquSQL.ToSQL()
 	var id int
-
-	err := database.QueryRow(sql).Scan(&id)
+	_, err := database.From("invoice").Insert().Rows(record).Returning("id").Executor().ScanVal(&id)
 	if err != nil {
-		fmt.Println(err)
+		return id, err
 	}
 
-	return id, err
+	return id, nil
 }
 
 // Update updates an existent Invoice on DB
@@ -178,20 +139,14 @@ func (repo *invoiceRepository) Update(invoice domain.Invoice) (int64, error) {
 		Where(goqu.Ex{"id": invoice.ID}).
 		Update().
 		Set(record)
-	sql, _, _ := goquSQL.ToSQL()
 
 	var rowsAffected int64 = -1
-
-	res, err := database.Exec(sql)
+	res, err := goquSQL.Executor().Exec()
 	if err != nil {
-		fmt.Println(err)
 		return rowsAffected, err
 	}
 
 	rowsAffected, err = res.RowsAffected()
-	if err != nil {
-		fmt.Println(err)
-	}
 
 	return rowsAffected, err
 }
